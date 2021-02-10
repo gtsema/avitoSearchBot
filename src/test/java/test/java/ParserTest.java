6451355d85126eac1cc88dcdfe10c319ea6f34f2
tsema <g.tsema@gmail.com>
2021-02-10 19:04:01 +0300

    prep test stucture

---
+++ src/test/java/test/java/ParserTest.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class ParserTest {
+
+    @Test
+    public void testParse() {
+        assertEquals(true, true);
+    }
+}
